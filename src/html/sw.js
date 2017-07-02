b1fa1fc32781dab20f236325031abe1066fa7e60
Simon Evans <simon.evans@torchbox.com>
2017-07-02 21:08:26 +0100

    Add Service Worker for offline support

---
+++ src/html/sw.js
@@ -0,0 +1,36 @@
+// Polyfill for Chrome caching
+importScripts('cache-polyfill.js');
+
+// Install the ServiceWorker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+
+    // Open a cache
+    caches.open('v1').then(function(cache) {
+
+      // Define what we want to cache
+      return cache.addAll([
+        '/',
+        'index.html',
+        'js/main.js',
+        'css/main.css',
+        'images/bin.svg'
+      ]);
+    })
+  );
+});
+
+// Use ServiceWorker (or not) to fetch data
+self.addEventListener('fetch', function(event) {
+
+  event.respondWith(
+
+    // Look for something in the cache that matches the request
+    caches.match(event.request).then(function(response) {
+
+      // If we find something, return it
+      // Otherwise, use the network instead
+      return response || fetch(event.request);
+    })
+  );
+});
